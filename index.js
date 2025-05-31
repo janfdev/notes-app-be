520a1a6e5c697850dfcd1ef108b18c0464610f2d
janfdev <fauzanrizqinoor@gmail.com>
2025-06-01 00:36:59 +0700

    Refactor backend: update connection string in config, enhance account creation endpoint, and implement user model

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
+require("dotenv").config();
+
+const config = require("./config.json");
+const mongoose = require("mongoose");
+
+mongoose.connect(config.connectionString);
+
 const express = require("express");
 const cors = require("cors");
 const app = express();
+
+const jwt = require("jsonwebtoken");
+const { authenticateToken } = require("./utilities");
 
 app.use(express.json());
 
 app.use(
   cors({
-    origin: "*",
+    origin: "*"
   })
 );
 
 app.get("/", (req, res) => {
   res.json({ data: "hello" });
+});
+
+// Create Account
+app.post("/create-account", async (req, res) => {
+  const { fullName, email, password } = req.body;
+
+  if (!fullName) {
+    res.status(400).json({
+      error: "Full name is required"
+    });
+  }
 });
 
 app.listen(8000);
